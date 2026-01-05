d376b5cfce326023c2f68776743ee7110f07bf0c
Lucas Eduardo <150375455+lucaseduardob@users.noreply.github.com>
2026-01-05 23:20:07 +0000

    adiciona pagina principal

---
+++ pages/index.js
@@ -0,0 +1,5 @@
+function Home() {
+    return <h1>Obrigado pelas conversas, pela atenção, pelos conselhos, pelo elogio que só vem de quem ama e, principalmente, por se importar comigo.</h1>
+}
+
+export default Home;
